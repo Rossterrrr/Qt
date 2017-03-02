1a1211727b998f04b56a82331de1d996ee99c3e9
equati0n <jbaird1@student.rcc.edu>
2017-03-02 00:36:29 -0800

    Stand Shoot, Crouch Shoot, Collision detection

---
+++ Cosmos_Game_V3/objecttype.h
@@ -0,0 +1,18 @@
+/*
+ * Author: Jonathan Baird
+ * Date: 2/20/2017
+ * Purpose: Cosmos Video Game
+*/
+
+#ifndef OBJECTTYPE_H
+#define OBJECTTYPE_H
+
+const int groundType = 1;
+const int parallaxLayerOneType = 2;
+const int platformType = 3;
+const int alienType = 4;
+const int pathType = 5;
+const int bulletType = 6;
+
+
+#endif // OBJECTTYPE_H
